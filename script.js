9071c4683b51e7ea88222249e3dad25d930de877
Joanna-Porter <jojomporter@gmail.com>
2025-04-02 15:22:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
- 
+function showit(var1){
+	console.log(var1)
+  document.getElementById("divvy1").style.display = "none";
+  document.getElementById("divvy2").style.display = "none";
+  document.getElementById("divvy3").style.display = "none";
+  document.getElementById("divvy4").style.display = "none";
+  document.getElementById("divvy5").style.display = "none";
+  document.getElementById("divvy6").style.display = "none";
+  document.getElementById(var1).style.display = "block";
+} 
